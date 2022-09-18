be458ceb9075ba586634aee83284b98594ccb8eb
Sagar Garg <sagar@risor.com>
2022-09-18 17:52:16 +0530

    progress bar added

--- app/src/main/java/com/example/memeshare/MainActivity.kt
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.memeshare
 
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.memeshare.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
@@ -41,12 +47,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadMeme() {
+        binding.progress.visibility = View.VISIBLE
         val memes = RetrofitClientInstance.memeInstance.getMeme()
         memes.enqueue(object : Callback<Meme> {
             override fun onResponse(call: Call<Meme>, response: Response<Meme>) {
                 runOnUiThread {
                     uri = response.body()?.url
-                    Glide.with(this@MainActivity).load(uri).into(binding.image)
+                    Glide.with(this@MainActivity).load(uri).listener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                            binding.progress.visibility = View.GONE
+                            return false
+                        }
+
+                        override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                            binding.progress.visibility = View.GONE
+                            return false
+                        }
+                    }).into(binding.image)
                 }
             }
 
